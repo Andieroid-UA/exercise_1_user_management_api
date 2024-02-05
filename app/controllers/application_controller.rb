350598a131b75b2ed10d8f78dc7a71335a7f17f2
Andres Astra <andieroid@arizona.edu>
2024-02-05 09:51:54 -0500

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    def create
+        @user = User.new(name: params[:name], email: params[:email])
+        if @user.save
+          render json: @user
+        else
+          render json: { error: "Unable to create user." }
+        end
+      end
 end
